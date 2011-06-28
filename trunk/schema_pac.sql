f3c08147db43a35e635f267c06294defd7f7eb1f
xjcook@gmail.com <xjcook@gmail.com@c7e9a4ac-53ce-d674-32be-389221c9695c>
2011-06-28 12:47:54 +0000

    Added view_profile_* procedures to package

--- trunk/schema_pac.sql
+++ trunk/schema_pac.sql
@@ -1,30 +1,43 @@
-CREATE OR REPLACE PACKAGE actions AS --spec
+CREATE OR REPLACE PACKAGE actions AS
   /**
   * Funkcia overi daneho pouzivatela.
-  * @param i_user
+  * @param i_username
   * @param i_password
   * @return Priznak, ci sa podarilo pouzivatela autentifikovat
   *   {*} TRUE Pouzivatela sa podarilo autentifikovat
   *   {*} FALSE Pouzivatela sa nepodarilo autentifikovat
   */
-  FUNCTION authenticate_user(i_user IN users.username%TYPE, i_password IN users.PASSWORD%TYPE) RETURN boolean;
+  FUNCTION authenticate_user(i_username IN users.username%TYPE, i_password IN users.PASSWORD%TYPE) RETURN boolean;
   
   /**
-  * Procedura vypise prehlad profilu uzivatela 
+  * Procedura vypise prehlad profilu uzivatela
+  * @param i_username
   */
   PROCEDURE view_profile_user(i_username IN users.username%TYPE);
+  
+  /**
+  * Procedura vypise prehlad objednavok uzivatela
+  * @param i_username
+  */
+  PROCEDURE view_orders_user(i_username IN users.username%TYPE);
+  
+  /**
+  * Procedura vypise prehlad rezervacii uzivatela
+  * @param i_username
+  */
+  PROCEDURE view_reservations_user(i_username IN users.username%TYPE);
 END actions;
 /
 
-CREATE OR REPLACE PACKAGE BODY actions AS --body
-  FUNCTION authenticate_user(i_user IN users.username%TYPE, i_password IN users.PASSWORD%TYPE) RETURN boolean IS
+CREATE OR REPLACE PACKAGE BODY actions AS
+  FUNCTION authenticate_user(i_username IN users.username%TYPE, i_password IN users.PASSWORD%TYPE) RETURN boolean IS
   BEGIN
-    IF i_user = i_password THEN
+    IF i_username = i_password THEN
       RETURN TRUE;
     ELSE
       RETURN FALSE;
     END IF;
-  END authenticate_user;
+  END;
   
   PROCEDURE view_profile_user(i_username IN users.username%TYPE) IS
   BEGIN
@@ -38,6 +51,30 @@ CREATE OR REPLACE PACKAGE BODY actions AS --body
       dbms_output.put_line('Telephone: ' || i.telephone);
       dbms_output.put_line('Credit: ' || i.credit);
     END LOOP;
-  END view_profile_user;
+  END;
+  
+  PROCEDURE view_orders_user(i_username IN users.username%TYPE) IS
+  BEGIN
+    FOR i IN (SELECT * FROM view_orders WHERE username = i_username) LOOP
+      dbms_output.put_line('ID: ' || i.ID);
+      dbms_output.put_line('Title: ' || i.title);
+      dbms_output.put_line('Year: ' || i.YEAR);
+      dbms_output.put_line('3D: ' || i.three_dimensional);
+      dbms_output.put_line('Discount: ' || i.discount);
+      dbms_output.put_line('Ordered: ' || i.ordered);
+    END LOOP;
+  END;
+  
+  PROCEDURE view_reservations_user(i_username IN users.username%TYPE) IS
+  BEGIN
+    FOR i IN (SELECT * FROM view_reservations WHERE username = i_username) LOOP
+      dbms_output.put_line('ID: ' || i.ID);
+      dbms_output.put_line('Title: ' || i.title);
+      dbms_output.put_line('Year: ' || i.YEAR);
+      dbms_output.put_line('3D: ' || i.three_dimensional);
+      dbms_output.put_line('Reserved: ' || i.reserved);
+      dbms_output.put_line('Valid to: ' || i.valid);
+    END LOOP;
+  END;
 END actions;
 /
